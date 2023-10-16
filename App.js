931de62ee721e29df9d2a11949f3c61dfedff27c
Tarique12344 <Tariqueelshamy11@gmail.com>
2023-10-16 17:54:16 -0400

    HW' '

---
+++ App.js
@@ -0,0 +1,29 @@
+
+var ourArray = [];
+for (var i = 0; i < 11; i++) {
+  ourArray.push(i);
+}
+console.log(ourArray);
+
+const myArray = [];
+
+let C = 5;
+while(C >= 11) {
+  myArray.push(C);
+  C--;
+}
+console.log(myArray);
+
+var ourArray = [0];
+
+for (var i = 1; i < 21; i += 1) {
+  ourArray.push(i);
+}
+console.log(ourArray);
+
+var ourArray = [];
+
+for (var i = 15; i > 1; i -= 2) {
+  ourArray.push(i);
+}
+console.log(ourArray);
